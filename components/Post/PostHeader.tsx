9e8d4d7c71be8c17dbb098e496cdcdf7d7cd58d2
JHSeo <qnfqnfqnf@gmail.com>
2021-11-09 23:15:28 +0900

    Change CategoryBox styles

--- components/Post/PostHeader.tsx
+++ components/Post/PostHeader.tsx
@@ -166,12 +166,13 @@ const WithIcon = styled('div', {
 });
 
 const CategoryBox = styled('a', {
-  px: '$2',
-  py: '$1',
+  px: '$3',
+  py: '$2',
   br: '$pill',
   bc: '$blue4',
   fontSize: '$sm',
   fontWeight: 'bold',
+  lineHeight: '1',
   color: '$blue11',
 
   '@hover': {
