a2934297e68bc2ee2799af6855c20e3ac48413c3
JHSeo <qnfqnfqnf@gmail.com>
2021-11-15 22:39:04 +0900

    Remove lazy loading image in markdown

--- components/Markdown/MarkdownRender.tsx
+++ components/Markdown/MarkdownRender.tsx
@@ -95,7 +95,6 @@ const components: ComponentType = {
             className="nextImage"
             src={src}
             alt={alt}
-            loading="lazy"
             layout="fill"
           />
         </ImageWrapper>
